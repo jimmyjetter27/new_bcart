4fd9c31e4032bb053c38b0a21a25ad58084c4243
James Atiah <jimmyjetter27@gmail.com>
2024-09-27 15:06:49 +0000

    first commit

---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username')->unique()->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->text('description')->nullable();
+            $table->string('location')->nullable();
+            $table->string('region')->nullable();
+            $table->string('type')->nullable();
+            $table->string('password')->nullable();
+            $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_name')->nullable();
+//            $table->string('profile_photo_path', 2048)->nullable();
+            $table->unsignedBigInteger('hiring_id')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user1 = User::create([
+            'first_name' => 'James',
+            'last_name' => 'Atiah',
+            'username' => 'jimmyjetter',
+            'email' => 'jimmyjetter27@gmail.com',
+            'email_verified_at' => now(),
+            'type' => 'App\\Models\\SuperAdmin',
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        $user2 = User::create([
+            'first_name' => 'Christian',
+            'last_name' => 'Amevor',
+            'username' => 'jumpingjacks',
+            'email' => 'amevchris@gmail.com',
+            'email_verified_at' => now(),
+            'type' => 'App\\Models\\SuperAdmin',
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        $user3 = User::create([
+            'first_name' => 'Neal',
+            'last_name' => 'Brakus',
+            'username' => 'Lone Wolf',
+            'email' => 'jacquelyn79@example.com',
+            'email_verified_at' => now(),
+            'type' => 'App\\Models\\Creative',
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        $user4 = User::create([
+            'first_name' => 'Laura',
+            'last_name' => 'Heller',
+            'username' => 'awoken monkey',
+            'email' => 'walton38@example.net',
+            'email_verified_at' => now(),
+            'type' => 'App\\Models\\RegularUser',
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::factory()->count(5)->create();
+    }
+}
