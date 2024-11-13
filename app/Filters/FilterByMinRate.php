b9dae4077666d7b7b57cc463fe4efff62fec4270
James Atiah <jimmyjetter27@gmail.com>
2024-11-13 11:15:55 +0000

    fixed minimum charge filter

--- app/Filters/FilterByMinRate.php
+++ app/Filters/FilterByMinRate.php
@@ -10,26 +10,29 @@ class FilterByMinRate implements Filter
 {
     public function __invoke(Builder $query, $value, string $property): Builder
     {
-        // Map the options to their corresponding conditions
-        switch ($value) {
-            case 'below_1000':
-                return $query->where('minimum_charge', '<', 1000);
+        return $query->whereHas('pricing', function ($q) use ($value) {
+            switch ($value) {
+                case 'below_1000':
+                    $q->where('minimum_charge', '<', 1000);
+                    break;
 
-            case '1000_to_2500':
-                return $query->whereBetween('minimum_charge', [1000, 2500]);
+                case '1000_to_2500':
+                    $q->whereBetween('minimum_charge', [1000, 2500]);
+                    break;
 
-            case '2500_to_5000':
-                return $query->whereBetween('minimum_charge', [2500, 5000]);
+                case '2500_to_5000':
+                    $q->whereBetween('minimum_charge', [2500, 5000]);
+                    break;
 
-            case 'above_5000':
-                return $query->where('minimum_charge', '>', 5000);
+                case 'above_5000':
+                    $q->where('minimum_charge', '>', 5000);
+                    break;
 
-            default:
-                // For custom rate or "Rate", expect a specific value and apply the condition
-                if (is_numeric($value)) {
-                    return $query->where('minimum_charge', '>=', $value);
-                }
-                return $query;
-        }
+                default:
+                    if (is_numeric($value)) {
+                        $q->where('minimum_charge', '>=', $value);
+                    }
+            }
+        });
     }
 }
