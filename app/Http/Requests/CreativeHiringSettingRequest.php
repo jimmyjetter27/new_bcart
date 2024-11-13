0c82dc56f1d984455829655a679ead1f412362f8
James Atiah <jimmyjetter27@gmail.com>
2024-11-13 13:24:54 +0000

    made creative_categories optional

--- app/Http/Requests/CreativeHiringSettingRequest.php
+++ app/Http/Requests/CreativeHiringSettingRequest.php
@@ -77,7 +77,7 @@ class CreativeHiringSettingRequest extends FormRequest
 //                'required_if:payment_details.momo_network,!=,null',
             ],
 
-            'creative_categories' => 'required|array',
+            'creative_categories' => 'required_if:creative_hire_status|array',
             'creative_categories.*' => 'exists:creative_categories,id',
         ];
     }
